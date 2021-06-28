2ea016bb2b687530850f0194087f8be923093b4c
PurnaChanduru <86110038+PurnaChanduru@users.noreply.github.com>
2021-06-28 16:59:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+function App(){
+  return(
+  <div>
+    <Header/ >
+  </div>
+  );
+  }
+  
+  export default App;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div class Name="note">
+      <h1>Javascript and React.js</h1>
+      <P> this was an amazing bootcamp taken up by shaurya sinha we covered everything from scratch inculdng javascript read.js,html. </P>
+    </div>
+  );
+}
+
+export default Info;
